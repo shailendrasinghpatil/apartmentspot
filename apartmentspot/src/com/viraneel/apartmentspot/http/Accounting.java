0743cd569a3f18788bb20198132175e5d4958ffa
rgujar <rgujar@gmail.com>
2013-08-05 16:53:27 +0000

    expense and voucher with types implemented - initial version

--- apartmentspot/src/com/viraneel/apartmentspot/http/Accounting.java
+++ apartmentspot/src/com/viraneel/apartmentspot/http/Accounting.java
@@ -27,6 +27,9 @@ import com.viraneel.apartmentspot.entities.Receipt;
 import com.viraneel.apartmentspot.entities.Refund;
 import com.viraneel.apartmentspot.entities.Vendor;
 import com.viraneel.apartmentspot.entities.VendorType;
+import com.viraneel.apartmentspot.entities.Voucher;
+import com.viraneel.apartmentspot.entities.Expense;
+import com.viraneel.apartmentspot.entities.ExpenseType;
 
 import com.viraneel.apartmentspot.entities.Society;
 import com.viraneel.apartmentspot.entities.Status;
@@ -60,6 +63,14 @@ public class Accounting extends BaseServlet {
 			deleteReceiptDetails(req, resp); 
 		} else if (userAction.equals("Get_BillingHead_Options")) {
 			getBillingHeadTypes(req, resp); 
+		} else if (userAction.equals("Get_Receipt_Details")) {
+			getReceiptDetails(req, resp);
+		} else if (userAction.equals("Delete_Receipt_Details")) {
+			deleteReceiptDetails(req, resp); 
+		} else if (userAction.equals("Get_BillingHead_Options")) {
+			getBillingHeadTypes(req, resp); 
+		} else if (userAction.equals("Get_Expense_Options")) {
+			getExpenseTypes(req, resp); 
 		} 
 	}
 
@@ -88,7 +99,28 @@ public class Accounting extends BaseServlet {
 		}
 	}
 	
-	
+	private void getExpenseTypes(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		String jsonStr = "";
+		System.out.println("called");
+		
+		if (null != userSessionProfile) {
+			List<ExpenseType> expenseTypes  = getExpenseType();
+			jsonStr = "{\"Result\":\"OK\",\"Options\":[";
+			for (ExpenseType Expensetype : expenseTypes) {
+				jsonStr = jsonStr + "{\"DisplayText\":\""
+						+ Expensetype.getExpenseType() + "\",\"Value\":\""
+						+ Expensetype.getExpenseType() + "\"},";
+			}
+
+			jsonStr = jsonStr.substring(0, jsonStr.length() - 1) + "]}";
+
+			System.out.println(jsonStr);
+			resp.getWriter().print(jsonStr);
+		}
+	}
 	private void getRefundDetails(HttpServletRequest req,
 			HttpServletResponse resp) throws IOException{
 		// TODO Auto-generated method stub
@@ -134,7 +166,7 @@ public class Accounting extends BaseServlet {
 			refund.setRefundReason(req.getParameter("refundReason"));
 			refund.setRefundPaymentDetails(req.getParameter("refundPaymentDetails"));
 			refund.setRefundTo(req.getParameter("refundTo"));
-			
+			refund.setRefundAmount(req.getParameter("refundAmount"));
 			
 			if (null != req.getParameter("refundDate")) {
 				Calendar refundDate = new GregorianCalendar();
@@ -159,7 +191,7 @@ public class Accounting extends BaseServlet {
 			}
 			
 						
-			refund.setRefundAmount(req.getParameter("refundAmount"));
+			
 			
 			pm.makePersistent(refund);
 			
@@ -203,7 +235,7 @@ public class Accounting extends BaseServlet {
 						refund.setRefundReason(req.getParameter("refundReason"));
 						refund.setRefundPaymentDetails(req.getParameter("refundPaymentDetails"));
 						refund.setRefundTo(req.getParameter("refundTo"));
-						
+						refund.setRefundAmount(req.getParameter("refundAmount"));
 						
 						if (null != req.getParameter("refundDate")) {
 							Calendar refundDate = new GregorianCalendar();
@@ -228,7 +260,7 @@ public class Accounting extends BaseServlet {
 
 						}
 						
-						refund.setRefundAmount(req.getParameter("refundAmount"));
+						
 											
 						pm.makePersistent(refund);
 						
@@ -359,7 +391,7 @@ public class Accounting extends BaseServlet {
 			
 			receipt.setReceiptDescription(req.getParameter("receiptDescription"));
 			receipt.setReceiptTo(req.getParameter("receiptTo"));
-			
+			receipt.setReceiptAmount(req.getParameter("receiptAmount"));
 			
 			if (null != req.getParameter("receiptDate")) {
 				Calendar receiptDate = new GregorianCalendar();
@@ -384,7 +416,7 @@ public class Accounting extends BaseServlet {
 			}
 			
 						
-			receipt.setReceiptAmount(req.getParameter("receiptAmount"));
+			
 			
 			pm.makePersistent(receipt);
 			
@@ -428,7 +460,7 @@ public class Accounting extends BaseServlet {
 						
 						receipt.setReceiptDescription(req.getParameter("receiptDescription"));
 						receipt.setReceiptTo(req.getParameter("receiptTo"));
-						
+						receipt.setReceiptAmount(req.getParameter("receiptAmount"));
 						
 						if (null != req.getParameter("receiptDate")) {
 							Calendar receiptDate = new GregorianCalendar();
@@ -452,9 +484,7 @@ public class Accounting extends BaseServlet {
 							}
 
 						}
-						
-						receipt.setReceiptAmount(req.getParameter("receiptAmount"));
-											
+															
 						pm.makePersistent(receipt);
 						
 						Society refreshedSoc = pm.getObjectById(Society.class,
@@ -535,6 +565,435 @@ public class Accounting extends BaseServlet {
 			System.out.println(jsonStr);
 			resp.getWriter().print(jsonStr);
 		}
+	}
+	
+	private void getVoucherDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException{
+		// TODO Auto-generated method stub
+		
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		String jsonStr = "";
+		if (null != userSessionProfile) {
+			Society soc = userSessionProfile.getCurrentSociety();
+			List<Voucher> vouchers = soc.getVoucher();
+			int startIndex = Integer.parseInt(req.getParameter("jtStartIndex"));
+			int pageSize = Integer.parseInt(req.getParameter("jtPageSize"));
+			String paramOrderBy = req.getParameter("jtSorting");
+
+			int endIndex = startIndex + pageSize;
+			if (endIndex >= vouchers.size()) {
+				endIndex = vouchers.size();
+			}
+			System.out.println("StartIndex=" + startIndex + " endIndex="
+					+ endIndex);
+			Query q = pm
+					.newQuery("select from com.viraneel.apartmentspot.entities.Voucher");
+			q.addExtension("datanucleus.query.evaluateInMemory", "true");
+			q.setCandidates(vouchers);
+			q.setRange(startIndex, endIndex);
+			q.setOrdering(paramOrderBy);
+			List<Voucher> voucher = (List<Voucher>) q.execute();
+
+			jsonStr = getJSONString(voucher);
+			jsonStr = "{\"Result\":\"OK\",\"Records\":" + jsonStr
+					+ ", \"TotalRecordCount\":\"" + vouchers.size() + "\"}";
+		}
+		resp.getWriter().print(jsonStr);
+	
+	}
+
+	private void processVoucherDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+	
+		if (null == req.getParameter("voucherID")) {
+			Voucher voucher = new Voucher();
+			
+			voucher.setVoucherNumber(req.getParameter("voucherNumber"));
+			voucher.setVoucherDescription(req.getParameter("voucherDescription"));
+			voucher.setVoucherFor(req.getParameter("voucherFor"));
+			voucher.setVoucherAmount(req.getParameter("voucherAmount"));
+			
+			if (null != req.getParameter("voucherDate")) {
+				Calendar voucherDate = new GregorianCalendar();
+				try {
+					voucherDate.setTime(new SimpleDateFormat("yyyy-dd-MM").parse(req
+							.getParameter("voucherDate")));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				voucher.setVoucherDate(voucherDate.getTime());
+			}
+			
+			if (null != req.getParameter("expenseType")) {
+
+				for (ExpenseType expenseType : getExpenseType()) {
+					if (expenseType.getExpenseType().equalsIgnoreCase(
+							req.getParameter("ExpenseType"))) {
+						voucher.setExpenseType(expenseType);
+						break;
+					}
+				}
+			}
+			
+						
+			voucher.setVoucherAmount(req.getParameter("voucherAmount"));
+			
+			pm.makePersistent(voucher);
+			
+			UserSessionProfile userSessionProfile = (UserSessionProfile) req
+					.getSession().getAttribute("userSessionProfile");
+			if (null != userSessionProfile) {
+				Society soc = userSessionProfile.getCurrentSociety();
+				List<Voucher> vouchers = soc.getVoucher();
+				if (vouchers == null) {
+					vouchers = new ArrayList<Voucher>();
+				}
+				vouchers.add(voucher);
+				soc.setVoucher(vouchers);
+				pm.makePersistent(soc);
+
+				Society refreshedSoc = pm.getObjectById(Society.class,
+						soc.getSocietyID());
+				userSessionProfile.setCurrentSociety(refreshedSoc);
+				req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+				String jsonStr = "{\"Result\":\"OK\",\"Record\":"
+						+ voucher.serializedJSON() + "}";
+				resp.getWriter().print(jsonStr);
+			}
+
+		} else {
+			UserSessionProfile userSessionProfile = (UserSessionProfile) req
+					.getSession().getAttribute("userSessionProfile");
+			if (null != userSessionProfile) {
+				Society soc = userSessionProfile.getCurrentSociety();
+				List<Voucher> vouchers = soc.getVoucher();
+				for (Voucher voucher : vouchers) {
+
+					Gson gson = new Gson();
+					System.out.println(req.getParameter("voucherID"));
+
+					long voucherID = Long.parseLong(req
+							.getParameter("voucherID"));
+					if (voucher.getVoucherID().getId() == voucherID) {
+						
+						voucher.setVoucherNumber(req.getParameter("voucherNumber"));
+						
+						voucher.setVoucherDescription(req.getParameter("voucherDescription"));
+						voucher.setVoucherFor(req.getParameter("voucherFor"));
+						
+						
+						if (null != req.getParameter("voucherDate")) {
+							Calendar voucherDate = new GregorianCalendar();
+							try {
+								voucherDate.setTime(new SimpleDateFormat("yyyy-dd-MM").parse(req
+										.getParameter("voucherDate")));
+							} catch (ParseException e) {
+								e.printStackTrace();
+							}
+							voucher.setVoucherDate(voucherDate.getTime());
+						}
+						
+						if (null != req.getParameter("voucherType")) {
+
+							for (ExpenseType expenseType : getExpenseType()) {
+								if (expenseType.getExpenseType().equalsIgnoreCase(
+										req.getParameter("expenseType"))) {
+									voucher.setExpenseType(expenseType);
+									break;
+								}
+							}
+
+						}
+						
+						voucher.setVoucherAmount(req.getParameter("voucherAmount"));
+											
+						pm.makePersistent(voucher);
+						
+						Society refreshedSoc = pm.getObjectById(Society.class,
+								soc.getSocietyID());
+						userSessionProfile.setCurrentSociety(refreshedSoc);
+						req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+						String jsonStr = "{\"Result\":\"OK\",\"Record\":"
+								+ voucher.serializedJSON() + "}";
+						resp.getWriter().print(jsonStr);
+						break;
+					}
+				}
+			}
+
+		}
+	
+	}
+
+	
+	private void deleteVoucherDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+		// TODO Auto-generated method stub
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		if (null != userSessionProfile) {
+			Society soc = userSessionProfile.getCurrentSociety();
+			List<Voucher> vouchers = soc.getVoucher();
+
+			Gson gson = new Gson();
+			System.out.println(req.getParameter("assetID[id]"));
+
+			long voucherID = Long.parseLong(req.getParameter("voucherID[id]"));
+			Key voucherKey = KeyFactory.createKey(Voucher.class.getSimpleName(),
+					voucherID);
+			Voucher voucher = pm.getObjectById(Voucher.class, voucherKey);
+			
+			if(vouchers != null){
+				vouchers.remove(voucher);
+			}
+			
+			Query q = pm.newQuery(Voucher.class);
+			q.setFilter("voucherID == voucherIDParam");
+			q.declareParameters(Key.class.getName() + " voucherIDParam");
+			q.deletePersistentAll(voucherKey);
+			
+			pm.makePersistent(soc);
+			pm.deletePersistent(voucher);
+			
+			System.out.println("Delete Successful");
+			Society refreshedSoc = pm.getObjectById(Society.class,
+					soc.getSocietyID());
+			userSessionProfile.setCurrentSociety(refreshedSoc);
+			req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+			String jsonStr = "{\"Result\":\"OK\"}";
+			resp.getWriter().print(jsonStr);
+		}
+		
+	}
+
+	private void getVoucherTypes(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		String jsonStr = "";
+		System.out.println("called");
+		
+		if (null != userSessionProfile) {
+			List<BillingHeadType> billingHeadTypes  = getBillingHeadType();
+			jsonStr = "{\"Result\":\"OK\",\"Options\":[";
+			for (BillingHeadType vouchertype : billingHeadTypes) {
+				jsonStr = jsonStr + "{\"DisplayText\":\""
+						+ vouchertype.getBillingHeadType() + "\",\"Value\":\""
+						+ vouchertype.getBillingHeadType() + "\"},";
+			}
+
+			jsonStr = jsonStr.substring(0, jsonStr.length() - 1) + "]}";
+
+			System.out.println(jsonStr);
+			resp.getWriter().print(jsonStr);
+		}
+	}
+
+	private void getExpenseDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException{
+		// TODO Auto-generated method stub
+		
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		String jsonStr = "";
+		if (null != userSessionProfile) {
+			Society soc = userSessionProfile.getCurrentSociety();
+			List<Expense> expenses = soc.getExpense();
+			int startIndex = Integer.parseInt(req.getParameter("jtStartIndex"));
+			int pageSize = Integer.parseInt(req.getParameter("jtPageSize"));
+			String paramOrderBy = req.getParameter("jtSorting");
+
+			int endIndex = startIndex + pageSize;
+			if (endIndex >= expenses.size()) {
+				endIndex = expenses.size();
+			}
+			System.out.println("StartIndex=" + startIndex + " endIndex="
+					+ endIndex);
+			Query q = pm
+					.newQuery("select from com.viraneel.apartmentspot.entities.Expense");
+			q.addExtension("datanucleus.query.evaluateInMemory", "true");
+			q.setCandidates(expenses);
+			q.setRange(startIndex, endIndex);
+			q.setOrdering(paramOrderBy);
+			List<Expense> expense = (List<Expense>) q.execute();
+
+			jsonStr = getJSONString(expense);
+			jsonStr = "{\"Result\":\"OK\",\"Records\":" + jsonStr
+					+ ", \"TotalRecordCount\":\"" + expenses.size() + "\"}";
+		}
+		resp.getWriter().print(jsonStr);
+	
+	}
+
+	private void processExpenseDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+	
+		if (null == req.getParameter("expenseID")) {
+			Expense expense = new Expense();
+			
+			expense.setExpenseNumber(req.getParameter("expenseNumber"));
+			expense.setExpenseDescription(req.getParameter("expenseDescription"));
+			expense.setExpenseFor(req.getParameter("expenseFor"));
+			expense.setExpenseAmount(req.getParameter("expenseAmount"));
+			expense.setTDSdeducted(req.getParameter("TDSdeducted"));
+			expense.setPanDeductee(req.getParameter("panDeductee"));
+			expense.setTanDeductee(req.getParameter("tanDeductee"));
+			
+			if (null != req.getParameter("expenseDate")) {
+				Calendar expenseDate = new GregorianCalendar();
+				try {
+					expenseDate.setTime(new SimpleDateFormat("yyyy-dd-MM").parse(req
+							.getParameter("expenseDate")));
+				} catch (ParseException e) {
+					e.printStackTrace();
+				}
+				expense.setExpenseDate(expenseDate.getTime());
+			}
+			
+			if (null != req.getParameter("expenseType")) {
+
+				for (ExpenseType expenseType : getExpenseType()) {
+					if (expenseType.getExpenseType().equalsIgnoreCase(
+							req.getParameter("ExpenseType"))) {
+						expense.setExpenseType(expenseType);
+						break;
+					}
+				}
+			}
+			
+						
+			expense.setExpenseAmount(req.getParameter("expenseAmount"));
+			
+			pm.makePersistent(expense);
+			
+			UserSessionProfile userSessionProfile = (UserSessionProfile) req
+					.getSession().getAttribute("userSessionProfile");
+			if (null != userSessionProfile) {
+				Society soc = userSessionProfile.getCurrentSociety();
+				List<Expense> expenses = soc.getExpense();
+				if (expenses == null) {
+					expenses = new ArrayList<Expense>();
+				}
+				expenses.add(expense);
+				soc.setExpense(expenses);
+				pm.makePersistent(soc);
+
+				Society refreshedSoc = pm.getObjectById(Society.class,
+						soc.getSocietyID());
+				userSessionProfile.setCurrentSociety(refreshedSoc);
+				req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+				String jsonStr = "{\"Result\":\"OK\",\"Record\":"
+						+ expense.serializedJSON() + "}";
+				resp.getWriter().print(jsonStr);
+			}
+
+		} else {
+			UserSessionProfile userSessionProfile = (UserSessionProfile) req
+					.getSession().getAttribute("userSessionProfile");
+			if (null != userSessionProfile) {
+				Society soc = userSessionProfile.getCurrentSociety();
+				List<Expense> expenses = soc.getExpense();
+				for (Expense expense : expenses) {
+
+					Gson gson = new Gson();
+					System.out.println(req.getParameter("expenseID"));
+
+					long expenseID = Long.parseLong(req
+							.getParameter("expenseID"));
+					if (expense.getExpenseID().getId() == expenseID) {
+						
+						expense.setExpenseNumber(req.getParameter("expenseNumber"));
+						
+						expense.setExpenseDescription(req.getParameter("expenseDescription"));
+						expense.setExpenseFor(req.getParameter("expenseFor"));
+						expense.setTDSdeducted(req.getParameter("TDSdeducted"));
+						expense.setPanDeductee(req.getParameter("panDeductee"));
+						expense.setTanDeductee(req.getParameter("tanDeductee"));
+						expense.setExpenseAmount(req.getParameter("expenseAmount"));
+						
+						if (null != req.getParameter("expenseDate")) {
+							Calendar expenseDate = new GregorianCalendar();
+							try {
+								expenseDate.setTime(new SimpleDateFormat("yyyy-dd-MM").parse(req
+										.getParameter("expenseDate")));
+							} catch (ParseException e) {
+								e.printStackTrace();
+							}
+							expense.setExpenseDate(expenseDate.getTime());
+						}
+						
+						if (null != req.getParameter("expenseType")) {
+
+							for (ExpenseType expenseType : getExpenseType()) {
+								if (expenseType.getExpenseType().equalsIgnoreCase(
+										req.getParameter("expenseType"))) {
+									expense.setExpenseType(expenseType);
+									break;
+								}
+							}
+
+						}
+						
+						
+											
+						pm.makePersistent(expense);
+						
+						Society refreshedSoc = pm.getObjectById(Society.class,
+								soc.getSocietyID());
+						userSessionProfile.setCurrentSociety(refreshedSoc);
+						req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+						String jsonStr = "{\"Result\":\"OK\",\"Record\":"
+								+ expense.serializedJSON() + "}";
+						resp.getWriter().print(jsonStr);
+						break;
+					}
+				}
+			}
+
+		}
+	
+	}
+
+	
+	private void deleteExpenseDetails(HttpServletRequest req,
+			HttpServletResponse resp) throws IOException {
+		// TODO Auto-generated method stub
+		UserSessionProfile userSessionProfile = (UserSessionProfile) req
+				.getSession().getAttribute("userSessionProfile");
+		if (null != userSessionProfile) {
+			Society soc = userSessionProfile.getCurrentSociety();
+			List<Expense> expenses = soc.getExpense();
+
+			Gson gson = new Gson();
+			System.out.println(req.getParameter("assetID[id]"));
+
+			long expenseID = Long.parseLong(req.getParameter("expenseID[id]"));
+			Key expenseKey = KeyFactory.createKey(Expense.class.getSimpleName(),
+					expenseID);
+			Expense expense = pm.getObjectById(Expense.class, expenseKey);
+			
+			if(expenses != null){
+				expenses.remove(expense);
+			}
+			
+			Query q = pm.newQuery(Expense.class);
+			q.setFilter("expenseID == expenseIDParam");
+			q.declareParameters(Key.class.getName() + " expenseIDParam");
+			q.deletePersistentAll(expenseKey);
+			
+			pm.makePersistent(soc);
+			pm.deletePersistent(expense);
+			
+			System.out.println("Delete Successful");
+			Society refreshedSoc = pm.getObjectById(Society.class,
+					soc.getSocietyID());
+			userSessionProfile.setCurrentSociety(refreshedSoc);
+			req.getSession().setAttribute("userSessionProfile", userSessionProfile);
+			String jsonStr = "{\"Result\":\"OK\"}";
+			resp.getWriter().print(jsonStr);
+		}
+		
 	}
 
 }
